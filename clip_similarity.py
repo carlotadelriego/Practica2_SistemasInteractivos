16f2dad0f7ea15ec618cf13040dcf87691afb36d
carlotadelriego <carlota.fernandez.01@uie.edu>
2025-10-28 22:12:01 +0100

    Agregar cálculo de embeddings de imágenes y textos utilizando el modelo CLIP

--- clip_similarity.py
+++ clip_similarity.py
@@ -30,3 +30,35 @@ clip_model.eval()
 # EMBEDDINGS
 image_embeddings = []
 text_embeddings = []
+
+for _, row in tqdm(df.iterrows(), total=len(df)):
+    # Imagen
+    img_path = os.path.join(DATASET_DIR, row["image_path"])
+    image = Image.open(img_path).convert("RGB")
+    inputs = clip_processor( # Procesamiento con CLIP 
+        text=[row["caption"]],
+        images=image,
+        return_tensors="pt",
+        padding=True
+    ).to(DEVICE)
+
+    with torch.no_grad(): # Obtener embeddings 
+        image_embeds = clip_model.get_image_features(inputs["pixel_values"]) 
+        text_embeds = clip_model.get_text_features(inputs["input_ids"])
+
+    # Normalización a través de la norma L2 
+    image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
+    text_embeds = text_embeds / text_embeds.norm(dim=-1, keepdim=True)
+
+    image_embeddings.append(image_embeds.cpu().numpy().flatten())
+    text_embeddings.append(text_embeds.cpu().numpy().flatten())
+
+image_embeddings = np.vstack(image_embeddings)
+text_embeddings = np.vstack(text_embeddings)
+
+print(f"Embeddings generados con CLIP: {image_embeddings.shape} (imágenes), {text_embeddings.shape} (textos)")
+
+
+
+# CÁLCULO DE SIMILITUD
+
